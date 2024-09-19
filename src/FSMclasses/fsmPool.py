63890cd9d8cc7e3e1df80a190e2779e7c88f07ab
Alex_kpl <kupelskiyaleksandr@gmail.com>
2024-09-19 20:55:25 +0500

    hide env

---
+++ src/FSMclasses/fsmPool.py
@@ -0,0 +1,24 @@
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+
+class FsmPool(StatesGroup):
+
+    #Первый скриншот
+    email_address = State()
+    name_plant = State()
+    problem_question = State()
+    #Второй скриншот
+
+    '''...'''
+
+    #Третий скриншот
+
+    """..."""
+
+
+    #Четвертый скриншот
+
+
+
+
--- src/Routers/PoolRouter.py
+++ src/Routers/PoolRouter.py
@@ -6,4 +6,3 @@ class PoolRouter:
     router = Router()
 
 
-class
